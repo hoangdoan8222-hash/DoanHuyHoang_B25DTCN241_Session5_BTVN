2cee93edb2ca173818a40ef9635c7883930bce54
hoangdoan8222-hash <hoangdoan8222@gmail.com>
2025-10-28 23:11:21 +0700

    Add files via upload

---
+++ Doan Huy Hoang_session05_BTVN6.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a, b;
+    char op;
+
+    printf("Nhap so thu nhat (a): ");
+    scanf("%f", &a);
+
+    printf("Nhap so thu hai (b): ");
+    scanf("%f", &b);
+
+    printf("Nhap toan tu (+, -, *, /): ");
+    scanf(" %c", &op); 
+    
+     return 0 ;
+}
