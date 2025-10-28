c25babb25a7d40b3df3923eef0184f3b74645f64
hoangdoan8222-hash <hoangdoan8222@gmail.com>
2025-10-28 23:10:46 +0700

    Add files via upload

---
+++ Doan Huy Hoang_session05_BTVN4..cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float thuNhap, thue;
+     printf("So Tien : ");
+    scanf("%f", &thuNhap);
+    if (thuNhap <= 5)
+        thue = thuNhap * 0.05;
+    else if (thuNhap <= 10)
+        thue = thuNhap * 0.10;
+    else
+        thue = thuNhap * 0.15;
+    printf("Thue thu nhap phai dong : %.2f\n", thue);
+
+    return 0;
+}
+
